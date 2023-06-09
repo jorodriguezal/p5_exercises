c6115bf6ca960cc2425802149e2bd3cc7e03d8eb
jorodriguezal <72422459+jorodriguezal@users.noreply.github.com>
2023-06-09 18:49:50 -0500

    add brightness and file handling

--- postproduction/sketch.js
+++ postproduction/sketch.js
@@ -4,44 +4,73 @@ let segments;
 let magnification;
 let lens_radius;
 let angelus;
+let distanceThresholdSlider;
+let img;
+let fileInput;
 
 function preload() {
-  quant = readShader('quant.frag',
-                            { varyings: Tree.texcoords2 });
-  img = loadImage('myImage.jpg');
+  quant = readShader("quant.frag", { varyings: Tree.texcoords2 });
+
+  bright = readShader("bright.frag", { varyings: Tree.texcoords2 });
+
+  img = loadImage("myImage.jpg");
 }
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(700, 500);
   quant_pg = createGraphics(width, height, WEBGL);
   quant_pg.colorMode(RGB, 1);
   quant_pg.textureMode(NORMAL);
   quant_pg.shader(quant);
- 
-//   magnification = createSlider(0.1, 9, 3, 0.1);
-//   magnification.position(width - 120, 10);
-//   magnification.style('width', '80px');
-//   magnification.input(() => {
-//     lens.setUniform('magnification', magnification.value())
-//     });
-//   lens.setUniform('magnification', magnification.value());
-//   lens_radius = createSlider(0, 500, 150, 1);
-//   lens_radius.position(width - 120, 35);
-//   lens_radius.style('width', '80px');
-//   lens_radius.input(() => {
-//     lens.setUniform('lens_radius', lens_radius.value())
-//     });
-//   lens.setUniform('lens_radius', lens_radius.value());
-segments = createSlider(1, 20, 1, 1);
-//   segments.position(10, 10);
+
+  bright_pg = createGraphics(width, height, WEBGL);
+  bright_pg.colorMode(RGB, 1);
+  bright_pg.textureMode(NORMAL);
+  bright_pg.shader(bright);
+
+  distanceThresholdSlider = createSlider(0, 1000, 100, 10);
+  distanceThresholdSlider.position(width - 120, 20);
+  distanceThresholdSlider.style("width", "80px");
+
+  quantization = createSlider(1, 50, 30, 1);
+  quantization.position(20, 20);
+
+  // create file input element
+  fileInput = createFileInput(handleFile);
+  fileInput.position(20, 50);
 }
 
 function draw() {
-  quant_pg.background(125);
-  quant.setUniform('texture', img);
-  quant.setUniform('uDivisor', segments.value());
-  quant_pg.emitResolution(quant);
-  pg = quant_pg;
-  pg.quad(-1, 1, 1, 1, 1, -1, -1, -1);
-  image(pg, 0, 0);
+  if (img) {
+    bright_pg.emitPointerPosition(bright, mouseX, mouseY, "iMouse");
+    bright.setUniform("texture", img);
+    bright.setUniform("distanceThreshold", distanceThresholdSlider.value());
+    pg = bright_pg;
+    pg.quad(-1, 1, 1, 1, 1, -1, -1, -1);
+
+    quant_pg.background(125);
+    quant.setUniform("texture", pg);
+    quant.setUniform("uDivisor", quantization.value());
+    quant_pg.emitResolution(quant);
+    pg = quant_pg;
+    pg.quad(-1, 1, 1, 1, 1, -1, -1, -1);
+
+    image(pg, 0, 0);
+  }
+}
+
+function handleFile(file) {
+  if (file.type === "image") {
+    img = loadImage(
+      file.data,
+      () => {
+        console.log("Image loaded successfully");
+      },
+      () => {
+        console.log("Error loading image");
+      }
+    );
+  } else {
+    console.log("Not an image file");
+  }
 }
